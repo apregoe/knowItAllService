7101eda7b9b0bdd4d36a102b6f850ae3fca146f7
Jonathon Shen <shenjona@usc.edu>
2017-10-05 14:22:32 -0700

    Added createPost files

---
+++ knowItAllService/knowItAllAPI/createPost/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CreatepostConfig(AppConfig):
+    name = 'createPost'
---
+++ knowItAllService/knowItAllAPI/createPost/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, include
+from . import views
+
+# . means importing from current directory
+
+urlpatterns = [
+    url(r'^$', views.index) # This is the base index for .../api/createPost
+]
