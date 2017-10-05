8077f289bccf6d2348be90ff0e87ea7be981ed56
Jonathon Shen <shenjona@usc.edu>
2017-10-04 21:07:49 -0700

    Finished setting up /api/ endpoint and created sub-API URLs like /api/register and /api/createPost

---
+++ knowItAllService/knowItAllAPI/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+from . import views
+# . means importing from current directory
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'), # This is the base index for .../api/
+    # url(r'^register/', include('register.urls')), # This is .../api/register
+    # url(r'^createPost/', include('createPost.urls')),  # This is .../api/createPost
+]
--- knowItAllService/knowItAllService/settings.py
+++ knowItAllService/knowItAllService/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'knowItAllAPI.apps.KnowitallapiConfig'
+    'knowItAllAPI'
 ]
 
 MIDDLEWARE = [
