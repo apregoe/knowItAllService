c3df884502f10eed5fea48ed838b2c7a446f1a3b
Nico Filip-Sanchez <nico.filipsan@gmail.com>
2017-10-12 09:09:42 -0700

    Updated DB reference

--- knowItAllService/knowItAllService/knowItAll.sql
+++ knowItAllService/knowItAllService/knowItAll.sql
@@ -3,62 +3,73 @@ CREATE DATABASE knowItAll;
 
 USE knowItAll;
 
--- Topic which is one of Academic, Food, Entertainment, Locations 
-CREATE TABLE Topic (
-    topicId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    topicName VARCHAR(50) NOT NULL
-);
-
--- Category within a Topic, e.g. 'Food Truck', 'Movie Theater', Park, Class
+-- 1 of the 4: Academic, Entertainment, Social, Location
 CREATE TABLE Category (
     categoryId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    topicId INT(11) NOT NULL,
-    categoryName VARCHAR(50) NOT NULL,
-    FOREIGN KEY (topicId)
-        REFERENCES Topic (topicId),
+    categoryName VARCHAR(20) NOT NULL,
 );
 
--- An member of a Category, e.g. CSCI103, 'Blaze Pizza', 
-CREATE TABLE CategoryMember (
-    categoryMemberId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+-- Ex. CSCI 310, Prof. Michael Schindler
+CREATE TABLE Topic (
+    topicId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     categoryId INT(11) NOT NULL,
-    categoryMemberName VARCHAR(50) NOT NULL,
+    title VARCHAR(50) NOT NULL,
     FOREIGN KEY (categoryId)
-        REFERENCES Category (categoryId),
+        REFERENCES Category (categoryId)
 );
 
 CREATE TABLE User (
     userId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    Token VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
     uscEmail VARCHAR(50) NOT NULL,
     verified INT(1)
 );
 
-CREATE TABLE UserRating (
-    userRatingId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+-- A UserReview is created every time a user reviews an Item.
+CREATE TABLE Review (
+    reviewId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userId INT(11) NOT NULL,
-    categoryMemberId INT(11) NOT NULL,
-    rating INT(11) NOT NULL,
+    topicId INT(11) NOT NULL,
+    rating INT(1) NOT NULL,
+    comment VARCHAR(256),
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (userID)
         REFERENCES User (userId),
-    FOREIGN KEY (categoryMemberId)
-        REFERENCES CategoryMember (categoryMemberId)
+    FOREIGN KEY (topicId)
+        REFERENCES Topic (topicId)
 );
 
 
 CREATE TABLE Poll (
-    packageID INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    pollId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     userID INT(11) NOT NULL,
-    title VARCHAR(64) NOT NULL,
-    description VARCHAR(1024) NOT NULL,
+    openForever INT(1)
+    endTime TIMESTAMP,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated  TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    text VARCHAR(64) NOT NULL,
     FOREIGN KEY (userID)
-        REFERENCES LinkItUser (userID)
+        REFERENCES User (userID)
 );
 
-- Create Topics
+CREATE TABLE PollChoice (
+    pollChoiceId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    pollId INT(11) NOT NULL,
+    text VARCHAR(64) NOT NULL,
+    FOREIGN KEY (pollId)
+        REFERENCES Poll (pollId)
+);
+
+CREATE TABLE Vote (
+    voteId INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    pollChoiceId INT(11) NOT NULL,
+    userId INT(11) NOT NULL,
+    FOREIGN KEY (pollChoiceId)
+        REFERENCES PollChoice (pollChoiceId)
+    FOREIGN KEY (userID)
+        REFERENCES User (userId),
+);
+
+- Create the 4 Main Categories that are always needed.
 INSERT INTO Topic (topicName)
     VALUES ('Academic');
 INSERT INTO Topic (topicName)
@@ -67,4 +78,4 @@ INSERT INTO Topic (topicName)
     VALUES ('Entertainment');
 INSERT INTO Topic (topicName)
     VALUES ('Locations'); 
-
+  
