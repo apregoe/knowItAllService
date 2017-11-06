362345cbf5954133deb2d9d1cfa2bb38e57dc051
Jonathon Shen <shenjona@usc.edu>
2017-11-05 18:36:59 -0800

    Minor bug fix

--- knowItAllService/knowItAllAPI/models.py
+++ knowItAllService/knowItAllAPI/models.py
@@ -106,7 +106,7 @@ class Tag(models.Model):
 class TagLinker(models.Model):
     tagID = models.ForeignKey(Tag, on_delete=models.CASCADE)
     type = models.CharField(max_length=200, default='') # topic, poll
-    pollID = models.ForeignKey(Poll, on_delete=models.CASCADE)
-    topicID = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    pollID = models.ForeignKey(Poll, on_delete=models.CASCADE, blank=True, null=True)
+    topicID = models.ForeignKey(Topic, on_delete=models.CASCADE, blank=True, null=True)
     def __str__(self):
         return self.tagID.title + " -- " + self.type
