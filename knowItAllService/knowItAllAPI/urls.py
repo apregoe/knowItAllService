d77a243fb9875aba92005551dbe29685c06e15f2
Jonathon Shen <shenjona@usc.edu>
2017-11-07 18:55:50 -0800

    Added login again

--- knowItAllService/knowItAllAPI/urls.py
+++ knowItAllService/knowItAllAPI/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'), # This is the base index for .../api/
 
     url(r'^register', include('knowItAllAPI.register.urls')), # This is .../api/register
+    url(r'^login', include('knowItAllAPI.login.urls')),
     url(r'^authenticate', include('knowItAllAPI.authenticate.urls')),
     url(r'^editProfile', include('knowItAllAPI.editProfile.urls')),
 
