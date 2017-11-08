2cd1e2fa6eb0f901c54c4ad24ade29e7887a944a
Jonathon Shen <shenjona@usc.edu>
2017-11-08 10:47:32 -0800

    Added pw to authenticate again

--- knowItAllService/knowItAllAPI/constants.py
+++ knowItAllService/knowItAllAPI/constants.py
@@ -216,9 +216,10 @@ def authenticate_UserAuthenticated(username):
             'message': "User authenticated successfully.",
             'data': {'username': username}}
 
-def authenticate_UserCheck(isVerified, username):
+def authenticate_UserCheck(isVerified, username, password):
     return {'status': 200,
             'username': username,
+            'password': password,
             'authenticated': str(isVerified)}
 
 # createTags
